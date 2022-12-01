ad14311a1f40c28479cba30fcb4b1f31d9ef48bb
Pietervb2 <p.c.m.vanbentum@student.tudelft.nl>
2022-12-01 14:34:04 +0100

    update

--- MIMO_assignment.m
+++ MIMO_assignment.m
@@ -1,8 +1,21 @@
-FWT = data.FWT;
-A=data.A;
-B=data.B;
-C=data.C;
-D=data.D;
+FWT = data.FWT(1:2,1:2);
+A=FWT.A;
+B=FWT.B;
+C=FWT.C;
+D=FWT.D;
 
-G = FWT.A;
-A
+s = tf('s');
+G = C*inv((s*eye(5)-A))*B;
+
+omega = [0, 0.6*pi];
+    for i =1:length(omega)
+        Gf=freqresp(G,omega(i));
+        RGAw(:,:,i)=Gf.*inv(Gf).';
+        RGAno(i)=sum(sum(abs(RGAw(:,:,i)-eye(2))));
+        
+    end
+ RGA = frd(RGAw,omega)
+%%
+% pzplot(FWT);
+bode(FWT)
+[Gm,Pm,Wcg,Wcp] = margin(FWT);
--- SISO_assignment.m
+++ SISO_assignment.m
@@ -21,7 +21,7 @@ stepinfo(-FWT(1,1));
 Plant = -FWT(1,1);
 Kp = 400;
 Ki = 50.2;
-Kd = 400;
+Kd = 0;
 Tf = 0.06;
 C = pid(Kp,Ki,Kd,Tf);
 
@@ -30,43 +30,22 @@ fbsys = feedback(Polecancel,1);
 bode(fbsys);
 step(fbsys);
 stepinfo(fbsys)
-margin(fbsys);
 
 %%
 % Try to use notch filter
-w1=0.2/(2*pi);
-w2=3.3/(2*pi);
-beta1 = 1;
-beta2 = 11;
-beta3 = 1;
-beta4 = 10;
-
-num1 = [1,w1*beta1,w1^2];
-den1 = [1, w1*beta2,w1^2];
-num2 = [1,w2*beta3,w2^2];
-den2 = [1, w2*beta3,w2^2];
-
-C_notch1 = tf(num1,den1);
-C_notch2 = tf(num2,den2);
-
-bode(C_notch1)
-%%
-% Extra pid
-Ku = 10;
-Tu = 31;
-% Ku = 10, Tu = 31 sec
-Kp = 0.50*Ku; 
-Ki = 0.40*Ku/Tu;
-Kd = 0.066*Ku*Tu;
-Tf= 1;
-
-K = pid(Kp, Ki, Kd, Tf);
-PoleCancel = minreal(Plant*C_notch1*C_notch2*K);
-sys_cl = feedback(PoleCancel,1);
-
-% bode(sys_cl);
-% margin(sys_cl);
-% [y,t] = step(sys_cl);
-stepinfo(sys_cl)
-step(sys_cl)
-% sserror=abs(1-y(end));
+% w1=0.2/(2*pi);
+% w2=3.3/(2*pi);
+% beta1 = 1;
+% beta2 = 11;
+% beta3 = 1;
+% beta4 = 10;
+% 
+% num1 = [1,w1*beta1,w1^2];
+% den1 = [1, w1*beta2,w1^2];
+% num2 = [1,w2*beta3,w2^2];
+% den2 = [1, w2*beta3,w2^2];
+% 
+% C_notch1 = tf(num1,den1);
+% C_notch2 = tf(num2,den2);
+% 
+% bode(C_notch1)
