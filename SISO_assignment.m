312ddbf5c4f391e741cabdcb185ff63fc1abe395
Pietervb2 <p.c.m.vanbentum@student.tudelft.nl>
2022-12-02 12:38:30 +0100

    exttra

--- SISO_assignment.m
+++ SISO_assignment.m
@@ -29,23 +29,14 @@ Polecancel = minreal(Plant*C); % To speed up the calculations
 fbsys = feedback(Polecancel,1);
 bode(fbsys);
 step(fbsys);
-stepinfo(fbsys)
+stepinfo(fbsys);
 
 %%
-% Try to use notch filter
-% w1=0.2/(2*pi);
-% w2=3.3/(2*pi);
-% beta1 = 1;
-% beta2 = 11;
-% beta3 = 1;
-% beta4 = 10;
-% 
-% num1 = [1,w1*beta1,w1^2];
-% den1 = [1, w1*beta2,w1^2];
-% num2 = [1,w2*beta3,w2^2];
-% den2 = [1, w2*beta3,w2^2];
-% 
-% C_notch1 = tf(num1,den1);
-% C_notch2 = tf(num2,den2);
-% 
-% bode(C_notch1)
+Plant = -FWT(1,3);
+
+Polecancel = minreal(Plant*C); % To speed up the calculations
+fbsys = feedback(Polecancel,1);
+bode(fbsys)
+% step(fbsys);
+stepinfo(fbsys);
+
